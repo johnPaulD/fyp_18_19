df3500468ec09d3660105bfd3da654f825345c12
John Paul Mendy <sc15jpm@leeds.ac.uk>
2019-05-10 08:39:59 +0100

    Complete Calendar to csv conversion

--- Program/main.py
+++ Program/main.py
@@ -5,91 +5,94 @@ import ics
 
 ### Convert any csv meeting a format to ics
 def csv2ics(filename):
-  try:
-    with open(filename+'.csv', newline='') as csvF:
-      table = csv.DictReader(csvF) #use a dict to easily find specific headers
-      cal = ics.Calendar()
-      for row in table:
-        # create a new calendar Event and fill it
-        ev = ics.Event()
-        #-# Get the different attributes necessary for an event in the Calendar #-#
-        if row['SUMMARY'] != None:
-          ev.name = row['SUMMARY']
-        else:
-          ev.name = ''
-        
-        # No calender event is complete without start and end date 
-        if row['DTSTART'] != None and row['DTEND'] != None:
-          form = 'MM-DD-YYYY hh:mm a' #Format
-          ev.begin = arrow.get(row['DTSTART'],form)
-          ev.end = arrow.get(row['DTEND'],form)
-        else:
-          raise Exception('Must Have both start and end dates')
-        try:
-          if row['NOTES'] != None:
-            ev.description = row['NOTES']
-          elif row['DESCRIPTION'] != None:
-            ev.description = row['DESCRIPTION']
-        finally:
-          if row['LOCATION'] != None:
-            ev.location = row['LOCATION']
-        
-        #Now save the event in the Calendar
-        cal.events.add(ev)
-      # After the 
-      f = open(filename+'.ics', 'w')
-      f.writelines(cal)
-      f.close()
-  # Error Handling options
-  except FileNotFoundError:
-    print("This file does not exist",filename)
-  except KeyError:
-    print('Check that the file head structure follows:\n',
-          'SUMMARY, DTSTART, DTEND, NOTES, LOCATION')
+    try:
+        with open(filename+'.csv', newline='') as csvF:
+            table = csv.DictReader(csvF) #use a dict to easily find specific headers
+            cal = ics.Calendar()
+            for row in table:
+                # create a new calendar Event and fill it
+                ev = ics.Event()
+                #-# Get the different attributes necessary for an event in the Calendar #-#
+                if row['SUMMARY'] != None:
+                    ev.name = row['SUMMARY']
+                else:
+                    ev.name = ''
+
+                # No calender event is complete without start and end date
+                if row['DTSTART'] != None and row['DTEND'] != None:
+                    form = 'MM-DD-YYYY hh:mm a' #Format
+                    ev.begin = arrow.get(row['DTSTART'],form)
+                    ev.end = arrow.get(row['DTEND'],form)
+                else:
+                    raise Exception('Must Have both start and end dates')
+                if row['NOTES'] != None:
+                    ev.description = row['NOTES']
+                elif row['DESCRIPTION'] != None:
+                    ev.description = row['DESCRIPTION']
+
+                if row['LOCATION'] != None:
+                    ev.location = row['LOCATION']
+                #'''
+                #Now save the event in the Calendar
+                cal.events.add(ev)
+            # create and save the ics file
+            f = open(filename+'.ics', 'w')
+            f.writelines(cal)
+            f.close()
+            print("Succesfully Created",'../New_Files/' + filename + '.ics')
+          #Error Handling options
+    except FileNotFoundError:
+        print("This file does not exist",filename)
+    except KeyError:
+        print('Check that the file head structure follows:\n',
+              'SUMMARY,DTSTART,DTEND,NOTES,LOCATION')
 
 
 #from ics import Calendar as Cal
 
-from icalendar import Calendar, Event
-
+from icalendar import Calendar, vDatetime
 def ics2csv(filename):
-  with open('test.ics','rb') as g:
-    gcal = Calendar.from_ical(g.read())
-    for component in gcal.walk():
-      if component.name == "VEVENT":
-        print("---------------------------------")
-        print(component.get('summary'))
-        print(component.get('dtstart').to_ical().decode())
-        print(component.get('dtend').to_ical().decode())
-        print(component.get('note'))
-  #g.close()
+    try:
+        with open(filename+'.ics','rb') as g:
+            gcal = Calendar.from_ical(g.read())
+            csvF = open(filename + '.csv', 'w')
+            lines = ['SUMMARY,DTSTART,DTEND,NOTES,LOCATION']
+            for component in gcal.walk():
+                if component.name == "VEVENT":
+                    line = []
+                    #f =
+                    line.append(component.get('summary'))
+                    # Extract and format the date accordingly
+                    st = vDatetime.from_ical(component.get('dtstart').to_ical().decode())
+                    line.append(st.strftime('%m-%d-%Y %I:%M %p'))
+                    en = vDatetime.from_ical(component.get('dtend').to_ical().decode())
+                    line.append(en.strftime('%m-%d-%Y %I:%M %p'))
+
+                    line.append(component.get('description'))
+                    line.append(component.get('location'))
+                    #make sure any data types are covered
+                    for i in range(len(line)):
+                        if line[i] == None:
+                            line[i] = ''
+                        else:
+                            line[i] = '"'+line[i]+'"'
+                    #print (line)
+                    lines.append(",".join(line))
+            csvF.writelines("\n".join(lines))
+            csvF.close()
+            print("Succesfully Created",'../New_Files/' + filename + '.csv')
+    except FileNotFoundError:
+        print("This file does not exist",filename)
+
+
 
 if __name__ == "__main__":
   # execute only if run as a script
-  #csv2ics('test')
   ics2csv('test')
-  
+  csv2ics('test')
 
-  '''
-  with open('test.ics', 'rb') as f:
-    filed = f.read()
-    c = Calendar(filed)
-    print(c)
-  #print(requests.get(url).text)
-  #print(c.events)
-  #
-  g = open('file.ics','rb')
-  gcal = Calendar.from_ical(g.read())
-  for component in gcal.walk():
-    if component.name == "VEVENT":
-      print "---------------------------------"
-      print component.get('summary')
-      print component.get('dtstart').to_ical()
-      print component.get('dtend').to_ical()
-  g.close()
-
-#'
-strTable = 
+'''
+strTable =
 ['<html>',
 '  <table>',
 '    <tr>',
@@ -104,12 +107,12 @@ for num in range(33,48):
  symb = chr(num)
  strRW = "<tr><td>"+str(symb)+ "</td><td>"+str(num)+"</td></tr>"
  strTable = strTable+strRW
- 
+
 strTable = strTable+"</table></html>"
- 
+
 hs = open("asciiCharHTMLTable.html", 'w')
 hs.write(strTable)
 hs.close()
- 
+
 print (strTable)
 '''
