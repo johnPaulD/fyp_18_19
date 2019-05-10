76708dac0028999e4f494d3c9a163218badbbf30
John Paul Mendy <sc15jpm@leeds.ac.uk>
2019-05-10 09:33:11 +0100

    single file left

---
+++ Program/csv2Latex.py
@@ -0,0 +1,26 @@
+#CSV to Latex
+import csv
+from pylatex import Document, Tabular, Section
+
+# Open the csv and also the latex files
+def csv2tex(filename):
+    doc = Document()
+    with open(filename+'.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        # get each row and convert it to Latex
+        header = next(reader)
+        size = '|l'*len(header)
+        size = size + '|'
+
+        #print(type(size))
+        with doc.create(Tabular(size)) as table:
+            table.add_hline()
+            table.add_row(header)
+            table.add_hline()
+            for row in reader:
+                table.add_row(row)
+                table.add_hline()
+    doc.generate_tex(filename)
+#test area
+if __name__ == "__main__":
+    csv2tex('test')
